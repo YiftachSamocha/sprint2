305ae0d24cb9b0b46626bdcfd64d31de67797fe5
Yiftach Samocha <yiftachsamo@gmail.com>
2024-03-15 14:55:33 +0200

    Create basic behavior; make the gallery disappear when image is clicked, make image cover canvas, add basic styling

---
+++ js/galleryController.js
@@ -0,0 +1,5 @@
+'use strict'
+function hideGallery(){
+    const gallery= document.querySelector('.gallery')
+    gallery.classList.add('hide')
+}
--- js/memeController.js
+++ js/memeController.js
@@ -1,1 +1,10 @@
 'use strict'
+const gElCanvas= document.querySelector('canvas')
+const gCtx= gElCanvas.getContext('2d')
+
+function renderMeme(image){
+    gElCanvas.height= image.clientHeight
+    gElCanvas.width= image.clientWidth
+    gCtx.drawImage(image, 0, 0, gElCanvas.width, gElCanvas.height)
+    hideGallery()
+}
